0be1cae0a217dcc3766488a78c0a5cebf77bcee2
RooyeKhat Media <rooyekhat.media@gmail.com>
2017-04-24 19:09:54 +0430

    first commit

---
+++ IGProtoBuff/IGProtoBuff.h
@@ -0,0 +1,25 @@
+//
+//  IGProtoBuff.h
+//  IGProtoBuff
+/*
+ * This is the source code of iGap for iOS
+ * It is licensed under GNU AGPL v3.0
+ * You should have received a copy of the license in this archive (see LICENSE).
+ * Copyright © 2017 , iGap - www.iGap.net
+ * iGap Messenger | Free, Fast and Secure instant messaging application
+ * The idea of the RooyeKhat Media Company - www.RooyeKhat.co
+ * All rights reserved.
+ */
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IGProtoBuff.
+FOUNDATION_EXPORT double IGProtoBuffVersionNumber;
+
+//! Project version string for IGProtoBuff.
+FOUNDATION_EXPORT const unsigned char IGProtoBuffVersionString[];
+
+
+
+
